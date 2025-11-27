40f9ce8d2f758f29fcd0e9861ca4d2fc6a95ba52
im-binary <pongpong0121@kakao.com>
2025-11-28 03:39:56 +0900

    fix: 배포 설정 추가

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -4,9 +4,13 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 
+const base: string =
+  process.env.NODE_ENV === 'production' ? '/front_7th_chapter3-1/' : '';
+
 // https://vite.dev/config/
 export default mergeConfig(
   defineConfig({
+    base,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
