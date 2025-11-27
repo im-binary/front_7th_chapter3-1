2c3b34c5dc1d784df19bf8386ddc4f05efc24732
im-binary <pongpong0121@kakao.com>
2025-11-27 16:49:21 +0900

    chore: prop 설명 추가

--- packages/after/src/components/ui/Input/types.ts
+++ packages/after/src/components/ui/Input/types.ts
@@ -1,9 +1,28 @@
 import type { VariantProps } from 'class-variance-authority';
 import type { InputHTMLAttributes } from 'react';
-import type { inputVariants } from '.';
+import type { inputVariants } from './index';
 
+/**
+ * Input 컴포넌트의 Props
+ */
 export interface InputProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'size'>,
     VariantProps<typeof inputVariants> {
+  /**
+   * Input의 타입
+   * @default 'text'
+   */
   type?: 'text' | 'email' | 'password' | 'number' | 'url';
+
+  /**
+   * Input의 크기
+   * @default 'md'
+   */
+  size?: 'sm' | 'md' | 'lg';
+
+  /**
+   * 에러 상태 표시
+   * @default false
+   */
+  error?: boolean;
 }
--- packages/after/src/components/ui/Select/types.ts
+++ packages/after/src/components/ui/Select/types.ts
@@ -1,10 +1,24 @@
 import * as SelectPrimitive from '@radix-ui/react-select';
 
+/**
+ * Select 루트 컴포넌트의 Props
+ */
 export interface SelectRootProps extends SelectPrimitive.SelectProps {
+  /**
+   * Select의 크기
+   * @default 'md'
+   */
   size?: 'sm' | 'md' | 'lg';
 }
 
+/**
+ * Select Trigger 컴포넌트의 Props
+ */
 export interface SelectTriggerProps extends SelectPrimitive.SelectTriggerProps {
+  /**
+   * 에러 상태 표시
+   * @default false
+   */
   error?: boolean;
 }
 
@@ -16,8 +30,17 @@ export interface SelectScrollDownButtonProps
 
 export interface SelectProps extends SelectPrimitive.SelectProps {}
 
+/**
+ * Select Content (드롭다운) 컴포넌트의 Props
+ */
 export interface SelectContentProps extends SelectPrimitive.SelectContentProps {
+  /**
+   * 드롭다운이 렌더링될 컨테이너
+   */
   container?: HTMLElement;
 }
 
+/**
+ * Select Item (옵션) 컴포넌트의 Props
+ */
 export interface SelectItemProps extends SelectPrimitive.SelectItemProps {}
